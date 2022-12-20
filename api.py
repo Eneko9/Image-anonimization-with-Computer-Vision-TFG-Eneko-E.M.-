318e63051d36cad7ceb060f629c8d9f0099ed59b
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2022-12-20 11:54:55 +0100

    Multiple file upload in the API

--- api.py
+++ api.py
@@ -12,6 +12,8 @@ from fastapi.responses import JSONResponse
 from pathlib import Path
 from pydantic import BaseModel
 import FacePlateBlur.facePlateDetector as fpd
+from typing import List
+from PIL import Image
 
 relative = os.getcwd()
 
@@ -30,15 +32,26 @@ def detector(request: Request):
     return templates.TemplateResponse("uploadImg.html", {"request":request})
 
 @app.post("/detector/response/",  response_class=HTMLResponse)
-def detectorResponse(request: Request, file: UploadFile = File(...)):
-    path = "static" + os.path.sep  + "pictures" + os.path.sep + f'{file.filename}'
-    with open( path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+def detectorResponse(request: Request, files: List[UploadFile]= File(...)):
+    resultsPath = relative + os.path.sep + "static" + os.path.sep + "results"
+    for f in os.listdir(resultsPath):
+        os.remove(os.path.join(resultsPath, f))
+    picPath = relative + os.path.sep + "static" + os.path.sep + "pictures"
+    for f in os.listdir(picPath):
+        os.remove(os.path.join(picPath, f))
+    zipPath = relative + os.path.sep + "static" + os.path.sep + "detections.zip"
+    if os.path.exists(zipPath):
+        os.remove(zipPath)
     yoloPath = os.getcwd() + "/yolov7"
     multiclassModel = fpd.loadYolo(yoloPath)
-    points = fpd.detection(path,multiclassModel)
-    img = fpd.hideObject(cv2.imread(path),points)
-    path =  relative + os.path.sep + "static" + os.path.sep + "results" + os.path.sep + f'{file.filename}'
-    cv2.imwrite(path,img)
-    path = "results/"+ f'{file.filename}'
+    for file in files:
+        path = "static" + os.path.sep  + "pictures" + os.path.sep + f'{file.filename}'
+        with open( path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        points, classes = fpd.detection(path,multiclassModel)
+        img = fpd.hideObject(cv2.imread(path),points,classes)
+        path =  relative + os.path.sep + "static" + os.path.sep + "results" + os.path.sep + f'{file.filename}'
+        cv2.imwrite(path,img)
+    shutil.make_archive( relative + os.path.sep + "static" + os.path.sep +'detections', 'zip', relative + os.path.sep + "static" + os.path.sep + "results")
+    path = "detections.zip"
     return templates.TemplateResponse("returnImg.html",{"request":request,"path": path})
