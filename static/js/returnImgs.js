3576511ad4bf44c3351dbcdaf37449899d132940
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2023-02-07 11:45:49 +0100

    rediseño terminado

--- static/js/returnImgs.js
+++ static/js/returnImgs.js
@@ -5,10 +5,7 @@ function loadImgs(){
     var imagenPrincipal = document.getElementById("imagenPrincipal");
     var filenames = document.getElementById("filenameArray").innerHTML;
 
-    
-
     var fnames = filenames.split(",");
-    alert(fnames);
     for (var i = 0; i <= fnames.length - 1; i++){
         fnames[i] = fnames[i].split("'")[1];
     }
@@ -22,8 +19,14 @@ function loadImgs(){
         thumb.classList.add("thumb");
         imagenesSecundarias.appendChild(thumb);
     }
-
+    thumbs = document.getElementsByClassName("thumb");
     
-    
-
+    //for every element with class "thumb" add an onclick event that changes the src of the main image
+    for (var i = 0; i < thumbs.length; i++) {
+        thumbs[i].addEventListener("click", function(){
+            imagenSecundaria = this.src;
+            this.src = imagenPrincipal.src;
+            imagenPrincipal.src = imagenSecundaria;
+        });
+    }
 }
