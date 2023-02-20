209e33cf5075eb981dbc82150cd247d0d656952c
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2023-02-20 12:01:03 +0100

    report bugs fixed

--- testing/generateReport.py
+++ testing/generateReport.py
@@ -4,25 +4,38 @@ results = r"C:\Users\eneko\GitHub\TFG-Eneko-Eguiguren-Teknei\static\annotationRe
 expected = r"C:\Users\eneko\GitHub\TFG-Eneko-Eguiguren-Teknei\testing\expected-annotations"
 
 def getInfoFromYoloAnnotations(line):
+    annotationArray = []
     if line != "":
         line = line.split(" ")
         line[-1] = line[-1].split("\n")[0]
-        classtype = line[0]
-        x = float(line[1])
-        y = float(line[2])
-        w = float(line[3])
-        h = float(line[4])
-    return classtype, x, y, w, h
+        annotationArray.extend([int(line[0]), float(line[1]), float(line[2]), float(line[3]), float(line[4])])
+    return annotationArray
+
+def check_annotation(expected, results):
+    # Comprobamos si las anotaciones tienen la misma clase
+    if expected[0] != results[0]:
+        return False
+    
+    # Obtenemos las coordenadas de los rectángulos delimitadores
+    exp_x, exp_y, exp_w, exp_h = expected[1:]
+    res_x, res_y, res_w, res_h = results[1:]
+    
+    # Comprobamos si el objeto esperado está dentro del objeto detectado
+    if (res_x - res_w/2) <= exp_x <= (res_x + res_w/2) and \
+       (res_y - res_h/2) <= exp_y <= (res_y + res_h/2) and \
+       exp_w <= res_w and exp_h <= res_h:
+        return True
+    else:
+        return False
 
 def compareResults(results, expected):
-    expectedNumerOfImgs = 100
+    expectedNumerOfImgs = 129
     with open("report.txt", "w") as report:
         precisions = [] # list of percentages of correct annotations
         generalPrecisions = []
         for folder in os.listdir(expected):
-            print(len(os.listdir(expected + os.path.sep + folder)))
-            print(f"-------------->IMAGES THAT CONTAIN {folder} ({len(folder)} in total)")
-            report.write(f"-------------->IMAGES THAT CONTAIN {folder} ({len(folder)} in total) \n")
+            print(f"-------------->IMAGES THAT CONTAIN {folder} ({len(os.listdir(expected + os.path.sep + folder))} in total)")
+            report.write(f"-------------->IMAGES THAT CONTAIN {folder} ({len(os.listdir(expected + os.path.sep + folder))} in total) \n")
             for f in os.listdir(results):
                 correct = 0
                 filename = f.split(".")[0]
@@ -33,25 +46,19 @@ def compareResults(results, expected):
                             expectedLines = exp.readlines()
                             for i in range(len(expectedLines)):
                                 detected = False
-                                exp_class, exp_x, exp_y, exp_w, exp_h = getInfoFromYoloAnnotations(expectedLines[i])
                                 for z in range(len(resultLines)):
-                                    res_class, res_x, res_y, res_w, res_h = getInfoFromYoloAnnotations(resultLines[z])
-                                    half_w = res_w / 2
-                                    half_h = res_h / 2
-                                    #si la clase es la misma, comprobar que los valores de x, y, w, h del expected estan dentro del resultado
-                                    if exp_class == res_class:
-                                        if exp_x >= res_x-half_w and exp_x <= res_x+half_w:
-                                                if exp_y >= res_y-half_h and exp_y <= res_y+half_h:
-                                                            correct += 1
-                                                            detected = True
-                                                            break
+                                    if check_annotation(getInfoFromYoloAnnotations(expectedLines[i]), getInfoFromYoloAnnotations(resultLines[z])):
+                                        correct += 1
+                                        detected = True
+                                        break
                                 if not detected:
                                     print(f"WARNING: The model has not detected one object ({i+1}) in image {filename} .")
                                     report.write(f"WARNING: The model has not detected one object ({i+1}) in image {filename} .\n")  
                             #get the precision  
+                            print(correct)
+                            print(len(expectedLines))
                             precision = correct / len(expectedLines) * 100
 
-                            print(f"expected lines --> {expectedLines}")
                             print(f"In image {filename} there are {correct} / {len(expectedLines)} correct annotations.")
                             report.write(f"In image {filename} there are {correct} / {len(expectedLines)} correct annotations.\n")
 
@@ -67,8 +74,8 @@ def compareResults(results, expected):
             report.write("/////////////////////////////////////\n")
             generalPrecisions.append(sum(precisions) / len(os.listdir(expected + os.path.sep + folder))) 
             print(sum(precisions) / len(precisions))
-            print(f"The average precision in {folder} is {generalPrecisions} %")
-            report.write(f"The average precision in {folder} is {generalPrecisions} %\n")
+            print(f"The average precision in {folder} is {sum(precisions) / len(os.listdir(expected + os.path.sep + folder))}% (in {len(precisions)} / {len(os.listdir(expected + os.path.sep + folder))} images was detected something)")
+            report.write(f"The average precision in {folder} is {sum(precisions) / len(os.listdir(expected + os.path.sep + folder))}% ({len(precisions)} / {len(os.listdir(expected + os.path.sep + folder))})\n")
 
             print("/////////////////////////////////////")
             report.write("/////////////////////////////////////\n")
